965ed0aad21907f338eb47e725352b14aabd668c
mmishak <mmishak.ya@gmail.com>
2016-12-18 22:29:50 +0300

    Отчет DDL

--- request_1.sql
+++ request_1.sql
@@ -1,17 +1,16 @@
 
+select id from (
+        SELECT FIRST 5 book_id as id FROM table_order_list
+        WHERE order_id IN 
+        (
+            SELECT id FROM table_order WHERE date_order BETWEEN date '01.10.2016' AND date '31.10.2016'
+        )
+        GROUP BY book_id
+        ORDER BY COUNT(book_id) DESC
 
-
-	SELECT FIRST 5 book_id FROM table_order_list
-	WHERE order_id IN 
-	(
-		SELECT id FROM table_order WHERE date_order BETWEEN date '01.10.2016' AND date '31.10.2016'
-	)
-	GROUP BY book_id
-	ORDER BY COUNT(book_id) DESC
-
-
-	UNION
-
-	SELECT FIRST 5 id FROM table_book
-	ORDER BY rating DESC
-	
+)
+    UNION
+select id from (
+    SELECT FIRST 5 id FROM table_book
+    ORDER BY rating DESC
+)
